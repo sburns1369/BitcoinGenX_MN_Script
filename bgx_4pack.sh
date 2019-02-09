4f5343fd3580eee79999cb44659072c400b71cde
sburns1369 <kiwishoepolish@yahoo.com>
2019-02-09 14:52:17 -0500

    Update bgx_4pack.sh

--- bgx_4pack.sh
+++ bgx_4pack.sh
@@ -14,10 +14,11 @@ COINDAEMONCLI=bitcoingenx-cli
 COINCORE=.bitcoingenx
 COINCONFIG=bitcoingenx.conf
 #Setting Colors
-BLUE='\033[0;96m'
+BLUE='[\033[0;36m\]'
 GREEN='\033[0;92m'
 RED='\033[0;91m'
 YELLOW='\033[0;93m'
+BOLD='\[\033[1;31m\]'
 #clear font
 CLEAR='\033[0m'
 
@@ -111,7 +112,7 @@ echo "listen=0" >> /home/${COINl}/.${COINl}/${COINCONFIG}
 echo "externalip=$(hostname  -I | cut -f1 -d' '):$COINPORT" >> /home/${COINl}/.${COINl}/${COINCONFIG}
 echo "masternodeprivkey=$privkey" >> /home/${COINl}/.${COINl}/${COINCONFIG}
 sleep 5
-echo "${BOLD}${RED}Launching First ${COIN3} Node"${CLEAR}
+echo "${BOLD}Launching First ${COIN3} Node"${CLEAR}
 ${COINDAEMON} -datadir=/home/${COINl}/.${COINl} -daemon
 sleep 60
 # Second Node Configuration and launch
@@ -149,7 +150,7 @@ echo "listen=0" >> /home/${COINl}3/.${COINl}/${COINCONFIG}
 echo "externalip=$(hostname  -I | cut -f1 -d' '):$COINPORT" >> /home/${COINl}3/.${COINl}/${COINCONFIG}
 echo "masternodeprivkey=$privkey3" >> /home/${COINl}3/.${COINl}/${COINCONFIG}
 sleep 5
-echo "${BOLD}${RED}Launching Third ${COIN3} Node"${CLEAR}
+echo "${BOLD}Launching Third ${COIN3} Node"${CLEAR}
 ${COINDAEMON} -datadir=/home/${COINl}3/.${COINl} -daemon
 sleep 60
 # Fourth Node Configuration and launch
@@ -168,31 +169,31 @@ echo "listen=0" >> /home/${COINl}4/.${COINl}/${COINCONFIG}
 echo "externalip=$(hostname  -I | cut -f1 -d' '):$COINPORT" >> /home/${COINl}4/.${COINl}/${COINCONFIG}
 echo "masternodeprivkey=$privkey3" >> /home/${COINl}4/.${COINl}/${COINCONFIG}
 sleep 5
-echo "${BOLD}${RED}Launching Fourth ${COIN3} Node"${CLEAR}
+echo "${RED}Launching Fourth ${COIN3} Node"${CLEAR}
 ${COINDAEMON} -datadir=/home/${COINl}4/.${COINl} -daemon
 sleep 60
 echo
-echo -e ${BOLD}${YELLOW}"Nodes Launched, please wait for them to sync".${CLEAR}
+echo -e ${BOLD}"Nodes Launched, please wait for them to sync".${CLEAR}
 echo
 echo -e "${BOLD}Your Masternodes are sync'ing this will take some time."${CLEAR}
 echo -e "While you wait you can configure your masternode.conf in your local wallet"${CLEAR}
 echo -e "The following data needs to be recorded in your local masternode configuration file:${CLEAR}"
-echo -e "${BOLD}${RED} Masternode_IP: $(hostname  -I | cut -f1 -d' '):${COINPORT}${CLEAR}"
+echo -e "${BOLD} Masternode_IP: $(hostname  -I | cut -f1 -d' '):${COINPORT}${CLEAR}"
 echo
-echo -e ${BOLD}${YELLOW}"If you become disconnected, you can check the status of sync'ing with"${CLEAR}
-echo -e "${BOLD}For MN#1- ${COINDAEMONCLI} -datadir=/home/${COINl}/.${COINl} mnsync status"${CLEAR}
-echo -e "${BOLD}For MN#2- ${COINDAEMONCLI} -datadir=/home/${COINl}2/.${COINl} mnsync status"${CLEAR}
-echo -e "${BOLD}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}3/.${COINl} mnsync status"${CLEAR}
-echo -e "${BOLD}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}4/.${COINl} mnsync status"${CLEAR}
+echo -e ${BOLD}"If you become disconnected, you can check the status of sync'ing with"${CLEAR}
+echo -e "${YELLOW}For MN#1- ${COINDAEMONCLI} -datadir=/home/${COINl}/.${COINl} mnsync status"${CLEAR}
+echo -e "${YELLOW}For MN#2- ${COINDAEMONCLI} -datadir=/home/${COINl}2/.${COINl} mnsync status"${CLEAR}
+echo -e "${YELLOW}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}3/.${COINl} mnsync status"${CLEAR}
+echo -e "${YELLOW}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}4/.${COINl} mnsync status"${CLEAR}
 echo -e
-echo -e ${BOLD}${YELLOW}"You can check the status of your ${COIN3} Masternode with"${CLEAR}
-echo -e "${BOLD}For MN#1- ${COINDAEMONCLI} -datadir=/home/${COINl}/.${COINl} masternode status"${CLEAR}
-echo -e "${BOLD}For MN#2- ${COINDAEMONCLI} -datadir=/home/${COINl}2/.${COINl} masternode status"${CLEAR}
-echo -e "${BOLD}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}3/.${COINl} masternode status"${CLEAR}
-echo -e "${BOLD}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}4/.${COINl} masternode status"${CLEAR}
+echo -e ${BOLD}"You can check the status of your ${COIN3} Masternode with"${CLEAR}
+echo -e "${YELLOW}For MN#1- ${COINDAEMONCLI} -datadir=/home/${COINl}/.${COINl} masternode status"${CLEAR}
+echo -e "${YELLOW}For MN#2- ${COINDAEMONCLI} -datadir=/home/${COINl}2/.${COINl} masternode status"${CLEAR}
+echo -e "${YELLOW}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}3/.${COINl} masternode status"${CLEAR}
+echo -e "${YELLOW}For MN#3- ${COINDAEMONCLI} -datadir=/home/${COINl}4/.${COINl} masternode status"${CLEAR}
 echo
 fi
-echo ${BOLD}${BLUE}"Your patronage is apprappreciated, tipping addresses"${CLEAR}
+echo ${BLUE}"Your patronage is apprappreciated, tipping addresses"${CLEAR}
 echo ${BLUE}"${COIN} address: BoEsUmcS3D9gVmdxvj7Che4wD1SAHa2zG9"${CLEAR}
 echo ${BLUE}"LTC address: MUdDdVr4Az1dVw47uC4srJ31Ksi5SNkC7H"${CLEAR}
 echo ${BLUE}"BTC address: 32FzghE1yUZRdDmCkj3bJ6vJyXxUVPKY93"${CLEAR}
