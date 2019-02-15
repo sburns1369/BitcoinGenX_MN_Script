0b2afba8f3c3cceb7912c31b517d04110ede4fc9
sburns1369 <kiwishoepolish@yahoo.com>
2019-02-15 09:05:20 -0500

    grammer

--- update_bgx_3pack.sh
+++ update_bgx_3pack.sh
@@ -14,7 +14,7 @@ COINDAEMONCLI=bitcoingenx-cli
 COINCORE=.bitcoingenx
 COINCONFIG=bitcoingenx.conf
 COINVERSION=1.6.0
-NODESL=Four
+NODESL=Three
 NODESN=3
 #AGREE = Agree to install Masternode
 #NULLREC = Agree to Record nullentrydev information
@@ -44,9 +44,9 @@ echo
 cd ~
 cd /usr/local/bin
 #Stop and kill old core
-${COINDAEMONCLI} -/home/${COINl}/.${COINl} stop
-${COINDAEMONCLI} -/home/${COINl}2/.${COINl} stop
-${COINDAEMONCLI} -/home/${COINl}3/.${COINl} stop
+${COINDAEMONCLI} -datadir=/home/${COINl}/.${COINl} stop
+${COINDAEMONCLI} -datadir=/home/${COINl}2/.${COINl} stop
+${COINDAEMONCLI} -datadir=/home/${COINl}3/.${COINl} stop
 sleep 2
     rm ${COINDAEMON}
     rm ${COINDAEMONCLI}
