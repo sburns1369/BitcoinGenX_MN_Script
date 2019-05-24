b7b54c59c88d61678781396a0907e23624ec08b9
sburns1369 <kiwishoepolish@yahoo.com>
2019-05-24 11:11:11 -0400

    .

---
+++ logo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+BLUE='\033[0;96m'
+GREEN='\033[0;92m'
+RED='\033[0;91m'
+YELLOW='\033[0;93m'
+clear
+echo
+echo -e "${BLUE} This Script was sponsored by : ${GREEN}"
+echo -e " ██████╗ ██╗████████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ";
+echo -e " ██╔══██╗██║╚══██╔══╝██╔════╝██╔═══██╗██║████╗  ██║ ";
+echo -e " ██████╔╝██║   ██║   ██║     ██║   ██║██║██╔██╗ ██║ ";
+echo -e " ██╔══██╗██║   ██║   ██║     ██║   ██║██║██║╚██╗██║ ";
+echo -e " ██████╔╝██║   ██║   ╚██████╗╚██████╔╝██║██║ ╚████║ ";
+echo -e " ╚═════╝ ╚═╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ";
+echo -e "   ${YELLOW}                                ██╗      ██╗              "
+echo -e "     ${RED}                              ╚██╗    ██╔╝              "
+echo -e "  ${YELLOW}     ██████╗ ███████╗███╗   ██╗   ╚██╗  ██╔╝    ";
+echo -e "  ${YELLOW}    ██╔════╝ ██╔════╝████╗  ██║${RED}    ╚██╗██╔╝    ";
+echo -e "  ${YELLOW}    ██║  ███╗█████╗  ██╔██╗ ██║     ╚███╔╝     ";
+echo -e "  ${YELLOW}    ██║   ██║██╔══╝  ██║╚██╗██║${RED}     ██╔██╗     ";
+echo -e "  ${YELLOW}    ╚██████╔╝███████╗██║ ╚████║    ██╔╝ ██╗    ";
+echo -e "  ${YELLOW}     ╚═════╝ ╚══════╝╚═╝  ╚═══╝${RED}   ██╔╝   ██╗    ";
+echo -e "  ${YELLOW}        * Green Protocol *       ██╔╝     ██╗    ";
+echo -e " ${RED}       * ZeroCoin Protocol *      ╚═╝      ╚═╝         "
+echo -e "${GREEN}     On the Web (BGX Wallets) : https://btcgenx.net/ "
+echo -e "${GREEN}      Official Discord : https://discord.gg/QQHgkuV "
+echo -e "${GREEN}Buy BGX : https://app.stex.com/en/basic-trade/pair/BTC/BGX "
+echo
+#ASCII art generated from patorjk.com
